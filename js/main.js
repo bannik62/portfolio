a249425e3f86627aff8ad44c13e43ea33edd5984
Yohann vanherzecke <djbk62@gmail.com>
2024-09-11 16:25:00 +0200

    changement header2Js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log("hello");
 
-    function arrowCalle(){
+    function arrowCalle() {
         const arrowScale = document.querySelector("#arrow")
         console.log(arrowScale);
-        arrowScale.classList.add("arrow")
-        
+        setTimeout(() => {
+            arrowScale.classList.add("arrow")
+        }, 1000);
+
+
     }
     arrowCalle()
-    
+    function moi() {
+        const moi = document.querySelector("#moi")
+        console.log(moi);
+        setTimeout(() => {
+            moi.classList.add("moi")
+        }, 2000);
+    }
+    moi()
+
 })
