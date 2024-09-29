cd183acb3dcb3c67523b3d3c0940a92436d48909
Yohann vanherzecke <52387576+bannik62@users.noreply.github.com>
2024-09-29 18:23:23 +0200

    ajout banner animé non stylisé

--- js/scriptTools.mjs
+++ js/scriptTools.mjs
@@ -16,21 +16,56 @@ export function formatDate() {
 console.log(formatDate());
 
 
-const canvas = document.getElementById('oscilloscope');
-const ctx = canvas.getContext('2d');
+// const canvas = document.getElementById('oscilloscope');
+// const ctx = canvas.getContext('2d');
 
-// Définir les dimensions du canvas
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// // Définir les dimensions du canvas
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
-// Couleurs
-const backgroundColor = '#00b300'; // Fond vert
-let lineColor = '#00ff7f'; // Vert clair
+// // Couleurs
+// const backgroundColor = '#00b300'; // Fond vert
+// let lineColor = '#00ff7f'; // Vert clair
 
-// Variables pour l'oscillation et modulation
-let time = 0;
-let amplitude = 50; // Amplitude initiale
-let frequency = 0.02; // Fréquence initiale
+// // Variables pour l'oscillation et modulation
+// let time = 0;
+// let amplitude = 50; // Amplitude initiale
+// let frequency = 0.02; // Fréquence initiale
 
 // Fonction pour dessiner l'oscilloscope
-// 
+// function drawOscilloscope() {
+//     // Effacer le canvas
+//     ctx.fillStyle = backgroundColor;
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//     ctx.beginPath();
+//     ctx.strokeStyle = lineColor;
+//     ctx.lineWidth = 2;
+
+//     const centerY = canvas.height / 2;
+
+//     // Dessiner la ligne d'oscillation avec modulation
+//     for (let x = 0; x < canvas.width; x++) {
+//         // Modulation de l'amplitude
+//         const modulatedAmplitude = amplitude * (1 + Math.sin((time + x) * 0.01)); // Variation d'amplitude
+//         const y = centerY + modulatedAmplitude * Math.sin((x + time) * frequency); // Fonction sinus
+//         ctx.lineTo(x, y);
+//     }
+
+//     ctx.stroke();
+//     ctx.closePath();
+
+//     // Incrémenter le temps pour faire défiler l'oscillation
+//     time += 8;
+
+//     // Modulation de la fréquence
+//     frequency += 1.0001; // Variation de fréquence au fil du temps
+
+//     // Modulation de la couleur
+//     // lineColor = `hsl(${(time * 0.5) % 360}, 100%, 50%)`; // Changer la couleur en utilisant HSL
+
+//     requestAnimationFrame(drawOscilloscope); // Demander le prochain cadre
+// }
+
+// // Commencer le dessin
+// drawOscilloscope();
